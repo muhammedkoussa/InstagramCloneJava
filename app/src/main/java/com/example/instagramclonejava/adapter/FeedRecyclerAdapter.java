03bc6f09dfea567a2b419625d2437569f2bfdacd
muhammedkoussa <76110583+muhammedkoussa@users.noreply.github.com>
2022-09-06 20:27:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/instagramclonejava/adapter/FeedRecyclerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.instagramclonejava.adapter;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.instagramclonejava.databinding.RecyclerRowBinding;
+import com.example.instagramclonejava.model.Post;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+public class FeedRecyclerAdapter extends RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder> {
+    private final ArrayList<Post> postArrayList;
+    public FeedRecyclerAdapter(ArrayList<Post> postArrayList) {
+        this.postArrayList = postArrayList;
+    }
+
+    static class PostHolder extends RecyclerView.ViewHolder {
+        RecyclerRowBinding recyclerRowBinding;
+        public PostHolder(@NonNull RecyclerRowBinding recyclerRowBinding) {
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding = recyclerRowBinding;
+        }
+    }
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new PostHolder(recyclerRowBinding);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+
+        holder.recyclerRowBinding.recyclerviewRowUseremailText.setText(postArrayList.get(position).email);
+        holder.recyclerRowBinding.recyclerviewRowCommentText.setText(postArrayList.get(position).comment);
+        Picasso.get().load(postArrayList.get(position).downloadUrl).into(holder.recyclerRowBinding.recyclerviewRowImageview);
+    }
+    @Override
+    public int getItemCount() {
+        return postArrayList.size();
+    }
+}
+
